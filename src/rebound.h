18cb3645e3784f556b3b1befe0d7bdbee3965394
silburt <silburt@astro.utoronto.ca>
2016-08-04 13:31:04 -0400

    added adaptive HSF routine

--- src/rebound.h
+++ src/rebound.h
@@ -227,6 +227,9 @@ struct reb_simulation_integrator_hermes {
     int* is_in_mini;
     int is_in_mini_Nmax;
     
+    int adaptive_hill_switch_factor;        ///< Flag (default==1) for automatically calculating the appropriate HSF value each iteration
+    double hill_switch_factor_floor;        ///< Optional HSF floor value (default=1), 
+    
     double* a_i;
     double* a_f;
     int a_Nmax;
