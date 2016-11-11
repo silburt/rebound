f03f7426bc8c831b480c643601d105fb00f35d65
silburt <silburt@astro.utoronto.ca>
2016-11-11 18:07:19 -0500

    better parameter space

--- examples/planetesimal_Kepler51/Plotting_runs_Npdttests.py
+++ examples/planetesimal_Kepler51/Plotting_runs_Npdttests.py
@@ -9,42 +9,59 @@ import numpy as np
 #import matplotlib.pyplot as plt
 
 #Specify what runs you want *****************************
-N_runs = 2
-np.random.seed(10)
-runs = []
-timestep = np.empty(0)
-Npl = np.empty(0)
-i=0
-while i < N_runs:
-    dt = np.random.uniform(1,4.5)
-    Np = np.random.uniform(1,5)
-    if (dt - 3)**2 + (Np - 3)**2 < 4:
-        timestep = np.append(timestep, dt-6)
-        Npl = np.append(Npl, Np)
-        i += 1
+
+#equal spacing
+t = np.logspace(-4.25,-2,6)
+N = np.logspace(2.6,4.5,6)
+array = []
+for tt in t:
+    for NN in N:
+        array.append((NN,tt))
+
+N_runs = len(array)
+Npl,timestep = zip(*array)
 
 #plt.scatter(Npl,timestep)
-#plt.xlabel('log10(N_planetesimals)')
-#plt.ylabel('log10(timestep)')
+#plt.xlabel('N_planetesimals')
+#plt.ylabel('timestep')
+#plt.xscale('log')
+#plt.yscale('log')
+#plt.xlim([1e2,1e5])
+#plt.ylim([1e-1,1e-5])
 #plt.savefig('output/sensitests.png')
 
+runs = []
 for i in xrange(0,N_runs):
-    dt = "{:.1e}".format(10**(timestep[i]))
-    Np = int(np.round(10**(Npl[i])))
+    dt = timestep[i]
+    Np = int(np.round(Npl[i]))
     seed = "{:.0f}".format(int(1000*random.random()))
-    name = 'output/sensitests_dt%s_Np%d_sd%s'%(dt,Np,seed)
+    name = 'output/sensitests_dt%.2e_Np%d_sd%s'%(timestep[i],Np,seed)
     runs.append((dt,Np,seed,name))
 
 os.system('make')
-
-length = len(runs)
 
 def execute(pars):
     os.system('./rebound '+str(pars[0])+' '+str(pars[1])+' '+str(pars[2])+' '+str(pars[3]))
 
 #Main multiprocess execution - Give sysname and letters of outer planets close to resonance
 if __name__== '__main__':
-    pool = mp.Pool(processes=length)
+    pool = mp.Pool(processes=N_runs)
     pool.map(execute, runs)
     pool.close()
     pool.join()
+
+
+
+#random generator
+#np.random.seed(10)
+#runs = []
+#timestep = np.empty(0)
+#Npl = np.empty(0)
+#i=0
+#while i < N_runs:
+#    dt = np.random.uniform(1,4.5)
+#    Np = np.random.uniform(1,5)
+#    if (dt - 3)**2 + (Np - 3)**2 < 4:
+#        timestep = np.append(timestep, dt-6)
+#        Npl = np.append(Npl, Np)
+#        i += 1
